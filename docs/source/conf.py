a0fc22dd12bbf1022b04ff27a75b39f1fdcbb41c
Motohiro <c201075@gmail.com>
2025-12-17 05:07:22 +0000

    Sphinx設定を更新し、必要な拡張機能を追加。ビルド出力を無視するための.gitignoreファイルを追加。

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,8 @@ author = 'motohiro'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_needs", 'sphinxcontrib.plantuml']
+plantuml = 'java -jar /usr/share/plantuml/plantuml.jar'
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +24,5 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['build']
